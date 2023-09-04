591393de6e5fc9a70a484cbd020bd1c09291f882
rishabh <therishabhrox@gmail.com>
2023-09-04 23:26:38 +0530

    setting up backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.CONN_STR, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        // useCreateIndex: true
+    }).then((con) => {
+        console.log(`MongoDB connected with server: ${con.connection.host}`);
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+
+module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 // config
 dotenv.config({path: 'backend/config/config.env'});
+
+// connecting to database
+connectDatabase();
 
 // creating server
 app.listen(process.env.PORT, () => {
