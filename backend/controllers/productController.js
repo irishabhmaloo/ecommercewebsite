036a41e850fa9d65f6bbe4c30ee8bec6a345f416
rishabh <therishabhrox@gmail.com>
2023-09-05 02:02:29 +0530

    product APIs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,93 @@
-// APIs
+const Product = require('../models/productModel');
 
-// getting all products
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        message: "Route is working fine"
-    })
+// CREATE product -- ADMIN
+exports.createProduct = async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            status: "success",
+            data: {
+                product
+            }
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: "fail",
+            message: "failure occured"
+        })
+    }
+}
+
+// GET all products
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(201).json({
+            status: "success",
+            data: {
+                products
+            }
+        })
+    } catch(err) {
+        res.status(400).json({
+            status: "fail",
+            message: "failure occured"
+        })
+    }
+};
+
+// GET product details
+exports.getProductDetails = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(201).json({
+            status: "success",
+            data: {
+                product
+            }
+        })
+    } catch(err) {
+        res.status(500).json({
+            status: "fail",
+            message: "Product not found"
+        })
+    }
+};
+
+// UPDATE product -- ADMIN
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            useFindAndModify: false,
+            runValidators: true
+        });
+        res.status(200).json({
+            status: "success",
+            data: {
+                product
+            }
+        })
+    } catch(err) {
+        res.status(500).json({
+            status: "fail",
+            message: "No product found"
+        })
+    }
+};
+
+// DELETE product -- ADMIN
+exports.deleteProduct = async (req, res) => {
+    try {
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            status: "success",
+            data: null
+        })
+    } catch(err) {
+        res.status(500).json({
+            status: "fail",
+            message: "No product found"
+        })
+    }
 };
---
+++ backend/models/productModel.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+// defining products schema
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Product name required"],
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, "Product description required"]
+    },
+    price: {
+        type: Number,
+        required: [true, "Product price required"],
+        maxlength: [8, "Price cannot exceed 8 characters"]
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    image: [{
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }],
+    category: {
+        type: String,
+        required: [true, "Product category required"],
+    },
+    stock: {
+        type: Number,
+        required: [true, "Product stock required"],
+        maxlength: [4, "Price cannot exceed 4 characters"],
+        default: 1
+    },
+    numberOfReviews: {
+        type: Number,
+        default: 0
+    },
+    reviews: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    createdAt: {
+        type: Data,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Product", productSchema);
