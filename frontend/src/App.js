038b73cf390ef3e63bab6595575559be3aff5d66
rishabh <therishabhrox@gmail.com>
2023-09-07 18:43:04 +0530

    Frontend - Header / Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import react from "react";
+import WebFont from "webfontloader";
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
 
 function App() {
+  react.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      }
+    })
+  });
+
   return (
-    <></>
+    <Router>
+      <Header />
+
+      <Footer />
+    </Router>
   );
 }
 
