5570c5d19a48d48c4dab27736aebe6c627e99ab4
rishabh <therishabhrox@gmail.com>
2024-01-02 21:28:38 +0530

    debug - 2/1/24

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -23,9 +23,9 @@ export const getProduct = (keyword = "", currentPage = 1, price = [0, 25000], ca
         try {
             dispatch({ type: ALL_PRODUCT_REQUEST });
 
-            let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
+            let link = `https://ecomm-server-ten.vercel.app/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`;
             if (category) {
-                link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
+                link = `https://ecomm-server-ten.vercel.app/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`;
             }
 
             // GET all products from BACKEND
