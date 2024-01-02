03ba5aa99d45de368caa37b76860e41a436c34db
rishabh <therishabhrox@gmail.com>
2024-01-03 01:52:18 +0530

    debug | cookies for cross-site - 3/1/24

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,7 +8,9 @@ const sendToken = (user, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httponly: true
+        httponly: true,
+        sameSite: 'None', // Set SameSite attribute to allow cross-site cookies
+        secure: process.env.NODE_ENV === 'production' // Add Secure attribute for HTTPS
     }
 
     res.status(statusCode).cookie('token', token, options).json({
