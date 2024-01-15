8784c46a8d4e95f638e6b3a81b601834f1ddc94c
rishabh <therishabhrox@gmail.com>
2024-01-15 17:34:16 +0530

    debug - Stripe API request 15/01/24

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,12 +35,14 @@ import About from "./components/layout/About/About.js";
 function App() {
 
   const backendUrl = process.env.REACT_APP_BACKEND_URL; 
+  const config = { withCredentials: true };
+
   const { isAuthenticated, user } = useSelector((state) => state.user);
 
   const [stripeApiKey, setStripeApiKey] = useState("");
 
   async function getStripeApiKey() {
-    const { data } = await axios.get(backendUrl + "/api/v1/stripeapikey");
+    const { data } = await axios.get(backendUrl + "/api/v1/stripeapikey", config);
 
     setStripeApiKey(data.stripeApiKey);
   }
